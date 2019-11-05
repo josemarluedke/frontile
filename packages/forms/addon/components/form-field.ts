542776f2438a097b1c703bd7b74d86c6ea64e59d
Josemar Luedke <josemarluedke@gmail.com>
2019-11-05 11:06:00 -0800

    [Forms] Create FormField component

---
+++ packages/forms/addon/components/form-field.ts
@@ -0,0 +1,19 @@
+import Component from '@glimmer/component';
+import { guidFor } from '@ember/object/internals';
+
+interface FormFieldArgs {
+  hasMargin?: boolean;
+  isSmall?: boolean;
+}
+
+export default class FormField extends Component<FormFieldArgs> {
+  id = guidFor(this);
+
+  get hintId(): string {
+    return this.id + '-hint';
+  }
+
+  get feedbackId(): string {
+    return this.id + '-feedback';
+  }
+}
