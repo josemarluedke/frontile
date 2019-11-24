f3ed417b7b5833467625c4b425410d0bef063ef7
Josemar Luedke <josemarluedke@gmail.com>
2019-11-23 20:27:20 -0800

    [Forms] Add low-level FormField::Input and FormField::Textarea components

---
+++ packages/forms/addon/components/form-field/input.ts
@@ -0,0 +1,34 @@
+import Component from '@glimmer/component';
+import { action } from '@ember/object';
+
+interface FormFieldInputArgs {
+  id?: string;
+  type?: string;
+
+  // Callback when oninput is triggered
+  onInput?: (value: string, event: InputEvent) => void;
+
+  // Callback when onchange is triggered
+  onChange?: (value: string, event: InputEvent) => void;
+}
+
+export default class FormFieldInput extends Component<FormFieldInputArgs> {
+  get type(): string {
+    if (typeof this.args.type === 'string') {
+      return this.args.type;
+    }
+    return 'text';
+  }
+
+  @action handleOnInput(event: InputEvent): void {
+    if (typeof this.args.onInput === 'function') {
+      this.args.onInput((event.target as HTMLInputElement).value, event);
+    }
+  }
+
+  @action handleOnChange(event: InputEvent): void {
+    if (typeof this.args.onChange === 'function') {
+      this.args.onChange((event.target as HTMLInputElement).value, event);
+    }
+  }
+}
