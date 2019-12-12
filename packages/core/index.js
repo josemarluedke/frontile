2edcb34a03126ed0775250f002c5dd27887ea8c9
Josemar Luedke <josemarluedke@gmail.com>
2019-12-11 20:19:54 -0800

    Add focus-visible to core

--- packages/core/index.js
+++ packages/core/index.js
@@ -1,5 +1,10 @@
 'use strict';
 
 module.exports = {
-  name: require('./package').name
+  name: require('./package').name,
+  included(...args) {
+    this._super.included.apply(this, args);
+
+    this.import('node_modules/focus-visible/dist/focus-visible.js');
+  }
 };
