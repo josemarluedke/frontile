3114b68be70fcfe242f9a762370b046ac5a39525
Josemar Luedke <josemarluedke@gmail.com>
2019-12-11 20:26:38 -0800

    Add VisuallyHidden component to core

---
+++ packages/core/addon/components/visually-hidden.ts
@@ -0,0 +1,5 @@
+import Component from '@glimmer/component';
+
+interface VisuallyHiddenArgs {}
+
+export default class VisuallyHidden extends Component<VisuallyHiddenArgs> {}
---
+++ packages/core/app/components/visually-hidden.js
@@ -0,0 +1,1 @@
+export { default } from '@frontile/core/components/visually-hidden';
