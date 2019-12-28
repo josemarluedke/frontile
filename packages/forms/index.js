014e9619f015bdb7f31b620bc3077df6c176a88f
Josemar Luedke <josemarluedke@gmail.com>
2019-12-27 17:04:41 -0800

    Add ember-power-select as dependency

--- packages/forms/index.js
+++ packages/forms/index.js
@@ -2,5 +2,27 @@
 
 module.exports = {
   name: require('./package').name,
-  tailwind: require('./addon/tailwind')
+  tailwind: require('./addon/tailwind'),
+  options: {
+    'ember-power-select': {
+      theme: false
+    }
+  },
+
+  included(includer, ...rest) {
+    const powerSelectOptions = includer.options['ember-power-select'] || {};
+    powerSelectOptions.theme = false;
+    includer.options['ember-power-select'] = powerSelectOptions;
+
+    this._super.included.apply(this, [includer, ...rest]);
+  },
+
+  // Ember does not call contentFor on nested addons, so we must call it
+  // manually.
+  contentFor(type, config) {
+    const emberPowerSelect = this.addons.find(
+      a => a.name === 'ember-power-select'
+    );
+    return emberPowerSelect.contentFor(type, config);
+  }
 };
