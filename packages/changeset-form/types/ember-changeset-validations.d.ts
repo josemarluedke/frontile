e4ff52c61caf1c108dc1ba4dc129016135aef092
Josemar Luedke <josemarluedke@gmail.com>
2020-01-11 13:16:13 -0800

    Add example form using ChangesetForm

---
+++ packages/changeset-form/types/ember-changeset-validations.d.ts
@@ -0,0 +1,36 @@
+declare module 'ember-changeset-validations' {
+  import { ValidationResult, ValidatorMap } from 'ember-changeset/types';
+
+  export default function lookupValidator(
+    validationMap: ValidatorMap
+  ): (params: {
+    key: string;
+    newValue: unknown;
+    oldValue: unknown;
+    changes: { [s: string]: unknown };
+    content: { [s: string]: unknown };
+  }) => string | boolean | [boolean] | string[] | Promise<ValidationResult>;
+}
+
+declare module 'ember-changeset-validations/validators/presence' {
+  import { ValidatorAction } from 'ember-changeset/types';
+  export default function validatePresence(
+    options?:
+      | boolean
+      | {
+          presence?: boolean;
+          ignoreBlank?: boolean;
+        }
+  ): ValidatorAction;
+}
+declare module 'ember-changeset-validations/validators/format' {
+  import { ValidatorAction } from 'ember-changeset/types';
+  export default function validateFormat(options?: {
+    allowBlank?: boolean;
+    type?: string;
+    inverse?: string;
+    regex?: RegExp | string;
+    allowNonTld?: boolean;
+    minTldLength?: number;
+  }): ValidatorAction;
+}
