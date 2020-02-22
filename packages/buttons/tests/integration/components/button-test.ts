c69b4bad114e446bb0a52ecd3fb382b801e58046
Josemar Luedke <josemarluedke@gmail.com>
2020-02-21 21:13:36 -0800

    Add tests for button classes

--- packages/buttons/tests/integration/components/button-test.ts
+++ packages/buttons/tests/integration/components/button-test.ts
@@ -19,4 +19,86 @@ module('Integration | Component | Button', function(hooks) {
 
     assert.dom('[data-test-id="button"]').hasAttribute('type', 'submit');
   });
+
+  module('Style classes', () => {
+    module('@appearance', () => {
+      test('it adds class for default appearance', async function(assert) {
+        await render(hbs`<Button data-test-id="button">My Button</Button>`);
+
+        assert.dom('[data-test-id="button"]').hasClass('btn');
+      });
+
+      test('it adds class for outlined appearance', async function(assert) {
+        await render(
+          hbs`<Button @appearance="outlined" data-test-id="button">My Button</Button>`
+        );
+
+        assert.dom('[data-test-id="button"]').doesNotHaveClass('btn');
+        assert.dom('[data-test-id="button"]').doesNotHaveClass('btn-minimal');
+        assert.dom('[data-test-id="button"]').hasClass('btn-outlined');
+      });
+
+      test('it adds class for outlined appearance', async function(assert) {
+        await render(
+          hbs`<Button @appearance="minimal" data-test-id="button">My Button</Button>`
+        );
+
+        assert.dom('[data-test-id="button"]').doesNotHaveClass('btn');
+        assert.dom('[data-test-id="button"]').doesNotHaveClass('btn-outlined');
+        assert.dom('[data-test-id="button"]').hasClass('btn-minimal');
+      });
+    });
+
+    module('@intent', () => {
+      test('it adds class for the an intent', async function(assert) {
+        await render(
+          hbs`<Button @intent="primary" data-test-id="button">My Button</Button>`
+        );
+
+        assert.dom('[data-test-id="button"]').hasClass('btn-primary');
+        assert
+          .dom('[data-test-id="button"]')
+          .doesNotHaveClass('btn-outlined-primary');
+        assert
+          .dom('[data-test-id="button"]')
+          .doesNotHaveClass('btn-minimal-primary');
+      });
+
+      test('it adds class for the an intent with appearance=outlined', async function(assert) {
+        await render(
+          hbs`<Button
+                @appearance="outlined"
+                @intent="primary"
+                data-test-id="button"
+              >
+                My Button
+             </Button>`
+        );
+
+        assert.dom('[data-test-id="button"]').hasClass('btn-outlined-primary');
+        assert.dom('[data-test-id="button"]').doesNotHaveClass('btn-primary');
+        assert
+          .dom('[data-test-id="button"]')
+          .doesNotHaveClass('btn-minimal-primary');
+      });
+
+      test('it adds class for the an intent with appearance=minimal', async function(assert) {
+        await render(
+          hbs`<Button
+                @appearance="minimal"
+                @intent="primary"
+                data-test-id="button"
+              >
+                My Button
+             </Button>`
+        );
+
+        assert.dom('[data-test-id="button"]').hasClass('btn-minimal-primary');
+        assert.dom('[data-test-id="button"]').doesNotHaveClass('btn-primary');
+        assert
+          .dom('[data-test-id="button"]')
+          .doesNotHaveClass('btn-outlined-primary');
+      });
+    });
+  });
 });
