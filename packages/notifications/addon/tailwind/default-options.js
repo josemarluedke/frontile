e974f1c374db0e9fb676490d7f7ce87034828b74
Josemar Luedke <josemarluedke@gmail.com>
2020-03-02 20:34:51 -0800

    Add config for notification styles

--- packages/notifications/addon/tailwind/default-options.js
+++ packages/notifications/addon/tailwind/default-options.js
@@ -3,23 +3,57 @@ const defaultTheme = require('tailwindcss/resolveConfig')(
 ).theme;
 
 const defaultConfig = {
-  // empty for now
+  textColor: defaultTheme.colors.white,
+  info: {
+    color: defaultTheme.colors.gray[900],
+    buttonHoverColor: defaultTheme.colors.gray[800]
+  },
+  success: {
+    color: defaultTheme.colors.blue[700],
+    buttonHoverColor: defaultTheme.colors.blue[800]
+  },
+  warning: {
+    color: defaultTheme.colors.yellow[600],
+    buttonHoverColor: defaultTheme.colors.yellow[700]
+  },
+  error: {
+    color: defaultTheme.colors.red[600],
+    buttonHoverColor: defaultTheme.colors.red[700]
+  }
 };
 
-function defaultOptions(/*{ config }*/) {
-  const placements = {
-    '&.top-left': { top: 0, left: 0 },
-    '&.top-center': { top: 0, left: '50%', transform: 'translateX(-50%)' },
-    '&.top-right': { top: 0, right: 0 },
-    '&.bottom-left': { bottom: 0, left: 0 },
-    '&.bottom-center': {
-      bottom: 0,
-      left: '50%',
-      transform: 'translateX(-50%)'
-    },
-    '&.bottom-right': { bottom: 0, right: 0 }
-  };
+const placements = {
+  '&.top-left': { top: 0, left: 0 },
+  '&.top-center': { top: 0, left: '50%', transform: 'translateX(-50%)' },
+  '&.top-right': { top: 0, right: 0 },
+  '&.bottom-left': { bottom: 0, left: 0 },
+  '&.bottom-center': {
+    bottom: 0,
+    left: '50%',
+    transform: 'translateX(-50%)'
+  },
+  '&.bottom-right': { bottom: 0, right: 0 }
+};
 
+function generateAppearance({ color, buttonHoverColor }) {
+  return {
+    card: {
+      backgroundColor: color,
+      closeBtn: {
+        '&:hover': {
+          backgroundColor: buttonHoverColor
+        }
+      },
+      customActionBtn: {
+        '&:hover': {
+          backgroundColor: buttonHoverColor
+        }
+      }
+    }
+  };
+}
+
+function defaultOptions({ config }) {
   const btnShared = {
     transitionProperty: defaultTheme.transitionProperty.default,
     transitionDuration: defaultTheme.transitionDuration[200],
@@ -45,7 +79,7 @@ function defaultOptions(/*{ config }*/) {
       card: {
         transitionProperty: defaultTheme.transitionProperty.default,
         transitionDuration: defaultTheme.transitionDuration[200],
-        color: defaultTheme.colors.white,
+        color: config.textColor,
         fontSize: defaultTheme.fontSize.sm,
         borderRadius: defaultTheme.borderRadius.default,
         boxShadow: defaultTheme.boxShadow.default,
@@ -76,7 +110,7 @@ function defaultOptions(/*{ config }*/) {
           height: '1em',
           width: '1em',
           backgroundRepeat: 'no-repeat',
-          iconColor: defaultTheme.colors.white,
+          iconColor: config.textColor,
           icon: iconColor =>
             `<svg fill="none" viewBox="0 0 24 24" stroke="${iconColor}" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12"></path></svg>`
         },
@@ -102,66 +136,10 @@ function defaultOptions(/*{ config }*/) {
         }
       }
     },
-    info: {
-      card: {
-        backgroundColor: defaultTheme.colors.gray[900],
-        closeBtn: {
-          '&:hover': {
-            backgroundColor: defaultTheme.colors.gray[800]
-          }
-        },
-        customActionBtn: {
-          '&:hover': {
-            backgroundColor: defaultTheme.colors.gray[800]
-          }
-        }
-      }
-    },
-    success: {
-      card: {
-        backgroundColor: defaultTheme.colors.blue[700],
-        closeBtn: {
-          '&:hover': {
-            backgroundColor: defaultTheme.colors.blue[800]
-          }
-        },
-        customActionBtn: {
-          '&:hover': {
-            backgroundColor: defaultTheme.colors.blue[800]
-          }
-        }
-      }
-    },
-    warning: {
-      card: {
-        backgroundColor: defaultTheme.colors.yellow[600],
-        closeBtn: {
-          '&:hover': {
-            backgroundColor: defaultTheme.colors.yellow[700]
-          }
-        },
-        customActionBtn: {
-          '&:hover': {
-            backgroundColor: defaultTheme.colors.yellow[700]
-          }
-        }
-      }
-    },
-    error: {
-      card: {
-        backgroundColor: defaultTheme.colors.red[600],
-        closeBtn: {
-          '&:hover': {
-            backgroundColor: defaultTheme.colors.red[700]
-          }
-        },
-        customActionBtn: {
-          '&:hover': {
-            backgroundColor: defaultTheme.colors.red[700]
-          }
-        }
-      }
-    }
+    info: generateAppearance(config.info),
+    success: generateAppearance(config.success),
+    warning: generateAppearance(config.warning),
+    error: generateAppearance(config.error)
   };
 }
 
