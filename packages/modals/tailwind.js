e8f81f5778eb263c16db837eb6336881257d1938
Josemar Luedke <josemarluedke@gmail.com>
2020-04-02 11:04:55 -0700

    Create @frontile/modals package

---
+++ packages/modals/tailwind.js
@@ -0,0 +1,25 @@
+const plugin = require('tailwindcss/plugin');
+const { resolve, isEmpty } = require('@frontile/tailwindcss-plugin-helpers');
+
+module.exports = plugin.withOptions(function (userConfig) {
+  return function ({ addComponents, theme }) {
+    const { options } = resolve(
+      '@frontile/modals',
+      require('./addon/tailwind/default-options'),
+      userConfig,
+      theme
+    );
+
+    function addTODO(options, modifier) {
+      if (isEmpty(options)) {
+        return;
+      }
+      addComponents({ [`.TODO${modifier}`]: options });
+    }
+
+    Object.keys(options).forEach((key) => {
+      const modifier = key === 'default' ? '' : `-${key}`;
+      addTODO(options[key].TODO, modifier);
+    });
+  };
+});
