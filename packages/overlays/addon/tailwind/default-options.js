ba511575577049738fa0955e03f4c1eb94e1a51f
Josemar Luedke <josemarluedke@gmail.com>
2020-04-10 18:01:30 -0700

    Overlay tailwind default options cleanup and use the same close icon in notifications

--- packages/overlays/addon/tailwind/default-options.js
+++ packages/overlays/addon/tailwind/default-options.js
@@ -7,8 +7,102 @@ const defaultConfig = {
 };
 
 function defaultOptions(/*{ config }*/) {
+  const inset0 = {
+    top: 0,
+    bottom: 0,
+    right: 0,
+    left: 0
+  };
+
   return {
     default: {
+      overlay: {
+        zIndex: 50,
+        jsIsOpen: {
+          overflow: 'hidden'
+        },
+
+        backdrop: {
+          ...inset0,
+          position: 'fixed',
+          backgroundColor: 'rgba(0, 0, 0, 0.45)',
+          userSelect: 'none'
+        },
+
+        content: {
+          ...inset0,
+          position: 'fixed',
+          overflow: 'auto',
+          '-webkit-overflow-scrolling': 'touch'
+        }
+      },
+      modal: {
+        backgroundColor: defaultTheme.colors.white,
+        borderRadius: defaultTheme.borderRadius.default,
+        boxShadow: defaultTheme.boxShadow.default,
+        marginBottom: defaultTheme.margin[24],
+        marginLeft: 'auto',
+        marginRight: 'auto',
+        marginTop: defaultTheme.spacing[24],
+        maxWidth: defaultTheme.maxWidth['xl'],
+        outline: 'none',
+        position: 'relative',
+
+        [`@media (max-width: ${defaultTheme.screens.sm})`]: {
+          maxWidth: `calc(100vw - ${defaultTheme.margin[4]})`
+        },
+
+        closeBtn: {
+          display: 'flex',
+          position: 'absolute',
+          fontSize: defaultTheme.fontSize.xl,
+          padding: defaultTheme.spacing[2],
+          top: defaultTheme.padding[2],
+          right: defaultTheme.padding[2],
+          transitionProperty: defaultTheme.transitionProperty.default,
+          transitionDuration: defaultTheme.transitionDuration[200],
+          borderRadius: defaultTheme.borderRadius.full,
+
+          '&:hover': {
+            backgroundColor: defaultTheme.colors.gray[200]
+          },
+
+          '&.focus-visible:focus': {
+            outline: 'none',
+            boxShadow: defaultTheme.boxShadow.outline
+          },
+
+          icon: {
+            height: '1em',
+            width: '1em',
+            backgroundRepeat: 'no-repeat',
+            iconColor: 'currentColor',
+            icon: (iconColor) =>
+              `<svg fill="none" stroke="${iconColor}" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="M6 18L18 6M6 6l12 12"></path></svg>`
+          }
+        },
+
+        header: {
+          fontWeight: defaultTheme.fontWeight.bold,
+          fontSize: defaultTheme.fontSize.xl,
+          padding: defaultTheme.padding[4],
+          borderTopRightRadius: defaultTheme.borderRadius.default,
+          borderTopLeftRadius: defaultTheme.borderRadius.default
+        },
+        body: {
+          padding: defaultTheme.padding[4]
+        },
+        footer: {
+          display: 'flex',
+          justifyContent: 'flex-end',
+          backgroundColor: defaultTheme.colors.gray[100],
+          borderTopWidth: defaultTheme.borderWidth.default,
+          borderTopColor: defaultTheme.borderColor.default,
+          padding: defaultTheme.padding[4],
+          borderBottomRightRadius: defaultTheme.borderRadius.default,
+          borderBottomLeftRadius: defaultTheme.borderRadius.default
+        }
+      },
       transitions: {
         fade: {
           enter: {
@@ -45,108 +139,6 @@ function defaultOptions(/*{ config }*/) {
             transform: 'scale(0.8)',
             transition: 'all 0.2s ease-in-out'
           }
-        }
-      },
-
-      overlay: {
-        zIndex: 50,
-        jsIsOpen: {
-          overflow: 'hidden'
-        },
-
-        backdrop: {
-          position: 'fixed',
-          top: 0,
-          bottom: 0,
-          right: 0,
-          left: 0,
-          backgroundColor: 'rgba(0, 0, 0, 0.45)',
-          userSelect: 'none'
-        },
-
-        content: {
-          position: 'fixed',
-          top: 0,
-          bottom: 0,
-          right: 0,
-          left: 0,
-          overflow: 'auto',
-          '-webkit-overflow-scrolling': 'touch'
-        }
-      },
-      modal: {
-        position: 'relative',
-        backgroundColor: defaultTheme.colors.white,
-        borderRadius: defaultTheme.borderRadius.default,
-        boxShadow: defaultTheme.boxShadow.default,
-        maxWidth: defaultTheme.maxWidth['xl'],
-        marginLeft: 'auto',
-        marginRight: 'auto',
-        outline: 'none',
-        marginTop: defaultTheme.spacing[24],
-        marginBottom: defaultTheme.margin[4],
-
-        [`@media (max-width: ${defaultTheme.screens.sm})`]: {
-          maxWidth: `calc(100vw - ${defaultTheme.margin[4]})`
-        },
-
-        closeBtn: {
-          display: 'flex',
-          position: 'absolute',
-          fontSize: defaultTheme.fontSize.xl,
-          padding: defaultTheme.spacing[2],
-          top: defaultTheme.padding[2],
-          right: defaultTheme.padding[2],
-          transitionProperty: defaultTheme.transitionProperty.default,
-          transitionDuration: defaultTheme.transitionDuration[200],
-          borderRadius: defaultTheme.borderRadius.full,
-
-          '&:hover': {
-            backgroundColor: defaultTheme.colors.gray[200]
-          },
-
-          '&.focus-visible:focus': {
-            outline: 'none',
-            boxShadow: defaultTheme.boxShadow.outline
-          },
-
-          icon: {
-            display: 'inline-block',
-            height: '1em',
-            width: '1em',
-            backgroundRepeat: 'no-repeat',
-            iconColor: 'currentColor',
-            icon: (iconColor) =>
-              `<svg fill="none" viewBox="0 0 24 24" stroke="${iconColor}" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12"></path></svg>`
-          }
-        },
-
-        header: {
-          fontWeight: defaultTheme.fontWeight.bold,
-          fontSize: defaultTheme.fontSize.xl,
-          paddingTop: defaultTheme.padding[4],
-          paddingBottom: defaultTheme.padding[4],
-          paddingRight: defaultTheme.padding[4],
-          paddingLeft: defaultTheme.padding[4]
-        },
-        body: {
-          paddingTop: defaultTheme.padding[4],
-          paddingBottom: defaultTheme.padding[8],
-          paddingRight: defaultTheme.padding[4],
-          paddingLeft: defaultTheme.padding[4]
-        },
-        footer: {
-          display: 'flex',
-          justifyContent: 'flex-end',
-          backgroundColor: defaultTheme.colors.gray[100],
-          borderTopWidth: defaultTheme.borderWidth.default,
-          borderTopColor: defaultTheme.borderColor.default,
-          paddingTop: defaultTheme.padding[4],
-          paddingBottom: defaultTheme.padding[4],
-          paddingRight: defaultTheme.padding[4],
-          paddingLeft: defaultTheme.padding[4],
-          borderBottomRightRadius: defaultTheme.borderRadius.default,
-          borderBottomLeftRadius: defaultTheme.borderRadius.default
         }
       }
     },
