2cc1abfbaa0b025d9758d933a40222052d88f51c
Josemar Luedke <josemarluedke@gmail.com>
2020-08-18 11:57:12 -0700

    Add fix ts-ignore ban

--- packages/changeset-form/tests/test-helper.ts
+++ packages/changeset-form/tests/test-helper.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/ban-ts-ignore */
 import Application from 'dummy/app';
 import config from 'dummy/config/environment';
 import { setApplication } from '@ember/test-helpers';
