250fcafac7e0a5cf5324b13f5bff0719bcc6f043
Josemar Luedke <josemarluedke@gmail.com>
2020-10-23 18:55:31 -0700

    Refactor core plugin to use new configuration

--- packages/core/tailwind/index.js
+++ packages/core/tailwind/index.js
@@ -1,49 +1,68 @@
 const plugin = require('tailwindcss/plugin');
 const {
-  resolve,
+  merge,
+  resolveComponents,
   isEmpty,
   kebabCase
 } = require('@frontile/tailwindcss-plugin-helpers');
 
-module.exports = plugin.withOptions(function (userConfig) {
+function addMultipartComponent(
+  addComponents,
+  baseSelector,
+  options,
+  modifier = ''
+) {
+  if (isEmpty(options)) {
+    return;
+  }
+
+  if (modifier !== '') {
+    baseSelector += `--${modifier}`;
+  }
+
+  const { baseStyle, variants, parts } = options;
+
+  let defaultVariant = {};
+  if (variants && !isEmpty(variants.default)) {
+    defaultVariant = variants.default;
+    delete variants.default;
+  }
+
+  addComponents({
+    [baseSelector]: merge(baseStyle, defaultVariant)
+  });
+
+  if (!isEmpty(parts)) {
+    Object.keys(parts).forEach((key) => {
+      addComponents({
+        [`${baseSelector}__${kebabCase(key)}`]: parts[key]
+      });
+    });
+  }
+
+  if (!isEmpty(variants)) {
+    Object.keys(variants).forEach((key) => {
+      addMultipartComponent(
+        addComponents,
+        baseSelector,
+        variants[key],
+        kebabCase(key)
+      );
+    });
+  }
+}
+
+module.exports = plugin.withOptions(function () {
   return function ({ addComponents, theme }) {
-    const { options } = resolve(
-      '@frontile/core',
-      require('./default-options'),
-      userConfig,
-      theme
+    const { components } = resolveComponents(
+      theme('frontile.core') || {},
+      require('./default-options')
     );
 
-    function addStylesFor(base, options, modifier = '') {
-      if (isEmpty(options)) {
-        return;
-      }
-
-      if (modifier !== '') {
-        base += `--${modifier}`;
-      }
-
-      const { baseStyle, variants, parts } = options;
-
-      addComponents({
-        [base]: baseStyle
-      });
-
-      if (!isEmpty(parts)) {
-        Object.keys(parts).forEach((key) => {
-          addComponents({
-            [`${base}__${kebabCase(key)}`]: parts[key]
-          });
-        });
-      }
-
-      if (!isEmpty(variants)) {
-        Object.keys(variants).forEach((key) => {
-          addStylesFor(base, variants[key], kebabCase(key));
-        });
-      }
-    }
-
-    addStylesFor('.close-button', options.closeButton);
+    addMultipartComponent(
+      addComponents,
+      '.close-button',
+      components.closeButton
+    );
   };
 });
