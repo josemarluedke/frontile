1213ccbdfa7736a13de975c98851160cb6385e96
Josemar Luedke <josemarluedke@gmail.com>
2020-11-20 21:05:25 -0800

    Add Docfy/Remark plugin to add prose class for markdown
    It unwraps HTML present in the markdown as well as Docfy Demos. This way we can use components and demos inside of the documentation without having the markdown styles affect.

--- site/.docfy-config.js
+++ site/.docfy-config.js
@@ -3,6 +3,45 @@ const autolinkHeadings = require('remark-autolink-headings');
 const highlight = require('remark-highlight.js');
 const codeImport = require('remark-code-import');
 
+const shouldUnproseNode = (node) => {
+  return (
+    ['html'].includes(node.type) ||
+    (node.type === 'code' &&
+      ['component', 'template', 'preview-template'].includes(node.meta))
+  );
+};
+
+// Credit: https://github.com/tailwindlabs/tailwindcss.com/blob/1234b4faded6c7a06b734c49c61257137b4acc9b/remark/withProse.js
+const withProse = (tree) => {
+  let insideProse = false;
+  tree.children = tree.children.flatMap((node, i) => {
+    if (insideProse && shouldUnproseNode(node)) {
+      insideProse = false;
+      return [{ type: 'html', value: '</div>' }, node];
+    }
+    if (!insideProse && !shouldUnproseNode(node)) {
+      insideProse = true;
+      return [
+        { type: 'html', value: '<div class="prose">' },
+        node,
+        ...(i === tree.children.length - 1
+          ? [{ type: 'html', value: '</div>' }]
+          : [])
+      ];
+    }
+    if (i === tree.children.length - 1 && insideProse) {
+      return [node, { type: 'html', value: '</div>' }];
+    }
+    return [node];
+  });
+};
+
+const docfyWithProse = (ctx) => {
+  ctx.pages.forEach((page) => {
+    withProse(page.ast);
+  });
+};
+
 module.exports = {
   tocMaxDepth: 3,
   remarkPlugins: [
@@ -15,6 +54,7 @@ module.exports = {
     codeImport,
     highlight
   ],
+  plugins: [docfyWithProse],
   sources: [
     {
       root: path.resolve(__dirname, '../docs'),
