4aa4cfd4efc27963e803ff8d8557e371683485aa
betocantu93 <betocantu93@gmail.com>
2020-12-02 23:47:00 -0600

    manual mode

--- packages/changeset-form/addon/components/changeset-form/index.ts
+++ packages/changeset-form/addon/components/changeset-form/index.ts
@@ -1,15 +1,10 @@
 import Component from '@glimmer/component';
 import { tracked } from '@glimmer/tracking';
-import { Changeset } from 'ember-changeset';
-import { BufferedChangeset, ValidatorMap } from 'ember-changeset/types';
+import { BufferedChangeset } from 'ember-changeset/types';
 import { action } from '@ember/object';
-import { assert } from '@ember/debug';
-import lookupValidator from 'ember-changeset-validations';
 
 interface ChangesetFormArgs {
-  model?: { [key: string]: unknown };
   changeset?: BufferedChangeset;
-  validations?: ValidatorMap;
 
   runExecuteInsteadOfSave?: boolean;
 
@@ -18,48 +13,24 @@ interface ChangesetFormArgs {
 }
 
 export default class ChangesetForm extends Component<ChangesetFormArgs> {
-  @tracked changeset!: BufferedChangeset;
-
   @tracked hasSubmitted = false;
-
-  @action
-  setup() {
-    if (typeof this.args.changeset === 'undefined') {
-      assert(
-        '@model must be defined on <ChangesetForm> component if you do not provide a @changeset argument',
-        typeof this.args.model !== 'undefined'
-      );
-
-      if (typeof this.args.validations !== 'undefined') {
-        this.changeset = Changeset(
-          this.args.model || {},
-          lookupValidator(this.args.validations || {}),
-          this.args.validations
-        );
-      } else {
-        this.changeset = Changeset(this.args.model || {});
-      }
-    } else {
-      this.changeset = this.args.changeset;
-    }
-  }
 
   @action
   async handleSubmit(event: Event): Promise<void> {
     event.preventDefault();
-    await this.changeset.validate();
+    await this.args.changeset.validate();
 
     this.hasSubmitted = true;
 
-    if (this.changeset.isInvalid) {
+    if (this.args.changeset.isInvalid) {
       return;
     }
 
     let result;
     if (this.args.runExecuteInsteadOfSave) {
-      result = this.changeset.execute();
+      result = this.args.changeset.execute();
     } else {
-      result = await this.changeset.save({});
+      result = await this.args.changeset.save({});
     }
 
     if (typeof this.args.onSubmit === 'function') {
@@ -72,7 +43,7 @@ export default class ChangesetForm extends Component<ChangesetFormArgs> {
     event.preventDefault();
     this.hasSubmitted = false;
 
-    const { data } = this.changeset.rollback();
+    const { data } = this.args.changeset.rollback();
     if (typeof this.args.onReset === 'function') {
       this.args.onReset(data, event);
     }
