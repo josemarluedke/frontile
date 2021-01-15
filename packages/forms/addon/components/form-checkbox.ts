dd2c311949ee7609fc3b1a629bd2e1ee59bc2814
Josemar Luedke <josemarluedke@gmail.com>
2021-01-14 20:13:45 -0800

    Add arg docs for form components

--- packages/forms/addon/components/form-checkbox.ts
+++ packages/forms/addon/components/form-checkbox.ts
@@ -2,17 +2,28 @@ import Component from '@glimmer/component';
 import { action } from '@ember/object';
 
 interface FormCheckboxArgs {
+  /** The input field label */
   label?: string;
+  /** A help text to be displayed */
   hint?: string;
-  checked?: boolean;
+  /**
+   * If the checkbox is checked.
+   * You must also pass `onChange` to update its value.
+   */
+  checked: boolean;
+  /** The name of the checkbox */
   name?: string;
+  /** CSS classes to be added in the container element */
   containerClass?: string;
+  /** The size */
   size?: 'sm' | 'lg';
+  /** Callback when onchange is triggered */
+  onChange: (value: boolean, event: Event) => void;
 
-  // Callback when onchange is triggered
-  onChange?: (value: boolean, event: Event) => void;
-
-  // internal function for InputRadioGroup
+  /**
+   * Internal function for InputCheckboxGroup
+   * @ignore
+   **/
   _parentOnChange?: (value: boolean, event: Event) => void;
 }
 
