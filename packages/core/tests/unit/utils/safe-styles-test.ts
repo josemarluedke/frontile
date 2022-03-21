04e55e99edffeba232a5078adee7205adf378b45
eugenioenko <eyakhnenko@underline.com>
2022-03-21 14:33:20 -0700

    deleted the moved tests from packages

--- packages/core/tests/unit/utils/safe-styles-test.ts
+++
@@ -1,9 +0,0 @@
-import safeStyles from '@frontile/core/utils/safe-styles';
-import { module, test } from 'qunit';
-
-module('Unit | Utility | safeStyles', function () {
-  test('it works', function (assert) {
-    const result = safeStyles({ height: '10px', overflow: 'hidden' });
-    assert.equal(result.toString(), 'height:10px;overflow:hidden');
-  });
-});
