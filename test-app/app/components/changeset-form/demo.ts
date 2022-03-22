e68053f24fc0ddc462355b2c128fc8a0a4c22f28
eugenioenko <eyakhnenko@underline.com>
2022-03-21 17:17:33 -0700

    consolidated package templates into test-app

---
+++ test-app/app/components/changeset-form/demo.ts
@@ -0,0 +1,38 @@
+import Component from '@glimmer/component';
+import validateFormat from 'ember-changeset-validations/validators/format';
+import validatePresence from 'ember-changeset-validations/validators/presence';
+import countries from './countries';
+
+export const myValidations = {
+  firstName: validatePresence(true),
+  lastName: validatePresence(true),
+  email: validateFormat({ type: 'email' }),
+  countries: validatePresence(true)
+};
+
+interface ExampleArgs {}
+
+export default class Example extends Component<ExampleArgs> {
+  validations = myValidations;
+  countries = countries.map((c) => c.name);
+
+  model = {
+    firstName: null,
+    lastName: null,
+    email: 'frontile@example.com',
+    comments: 'Changeset forms are great! 🚀',
+    countries: [
+      'Brazil',
+      'India',
+      'Israel',
+      'Japan',
+      'United Kingdom',
+      'United States'
+    ],
+
+    hungry: true,
+    humble: true,
+    curious: true,
+    favoriteMeal: 'lunch'
+  };
+}
