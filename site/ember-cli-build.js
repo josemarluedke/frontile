da3a5eab31a1dcb05ffd34c5ad5093eae1988d3b
eugenioenko <eyakhnenko@underline.com>
2022-03-22 18:01:45 -0700

    removed unused purgecss options

--- site/ember-cli-build.js
+++ site/ember-cli-build.js
@@ -4,54 +4,6 @@ process.env.EXPERIMENTAL_RENDER_MODE_SERIALIZE = true;
 
 const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 const path = require('path');
-const env = EmberApp.env();
-
-const purgecssOptions = {
-  content: [
-    './app/index.html',
-    './app/**/*.hbs',
-    './lib/docfy-theme/addon/**/*.hbs',
-    '../**/*.md',
-    './node_modules/**/*.hbs',
-    '../node_modules/**/*.hbs'
-  ],
-  defaultExtractor: (content) => {
-    // Capture as liberally as possible, including things like `h-(screen-1.5)`
-    const broadMatches = content.match(/[^<>"'`\s]*[^<>"'`\s:]/g) || [];
-
-    // Capture classes within other delimiters like .block(class="w-1/2") in Pug
-    const innerMatches = content.match(/[^<>"'`\s.()]*[^<>"'`\s.():]/g) || [];
-
-    return broadMatches.concat(innerMatches);
-  },
-  whitelistPatterns: [
-    /js-/,
-    /mode-dark/,
-    /^close-button/,
-    /^sr-only/,
-    /^form-field-checkbox/,
-    /^form-field-feedback/,
-    /^form-field-hint/,
-    /^form-field-input/,
-    /^form-field-label/,
-    /^form-field-radio/,
-    /^form-field-textarea/,
-    /^form-input/,
-    /^form-textarea/,
-    /^form-select/,
-    /^form-checkbox/,
-    /^form-radio/,
-    /^form-checkbox-group/,
-    /^form-radio-group/,
-    /^notifications-container/,
-    /^notification-card/,
-    /^notification-transition/,
-    /^overlay/,
-    /^modal/,
-    /^drawer/,
-    /^btn/
-  ]
-};
 
 const postcssPlugins = [
   {
@@ -64,10 +16,6 @@ const postcssPlugins = [
   require('postcss-nested'),
   require('autoprefixer')
 ];
-if (env !== 'development' || process.env.PURGE_CSS === 'true') {
-  const purgecss = require('@fullhuman/postcss-purgecss')(purgecssOptions);
-  postcssPlugins.push(purgecss);
-}
 
 module.exports = function (defaults) {
   const app = new EmberApp(defaults, {
