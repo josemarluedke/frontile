405ad5ba6d894a9e7b4255a2df1856f51f19e240
eugenioenko <eyakhnenko@underline.com>
2022-04-06 13:55:02 -0700

    updated overlays

--- packages/overlays/addon/components/overlay.ts
+++ packages/overlays/addon/components/overlay.ts
@@ -4,108 +4,111 @@ import { action } from '@ember/object';
 import { later } from '@ember/runloop';
 import { getDOM, getElementById } from '../-private/dom';
 
-export interface OverlayArgs {
-  /**
-   * Whether to render in place or in the specified/default destination
-   *
-   * @defaultValue false
-   */
-  renderInPlace?: boolean;
+export interface OverlaySignature {
+  Args: {
+    /**
+     * Whether to render in place or in the specified/default destination
+     *
+     * @defaultValue false
+     */
+    renderInPlace?: boolean;
 
-  /**
-   * The destination where the overlay will be inserted, defaults to
-   * `document.body`
-   *
-   * @defaultValue undefined
-   */
-  destinationElementId?: string;
+    /**
+     * The destination where the overlay will be inserted, defaults to
+     * `document.body`
+     *
+     * @defaultValue undefined
+     */
+    destinationElementId?: string;
 
-  /**
-   * Duration of the animation
-   *
-   * @defaultValue 200
-   */
-  transitionDuration?: number;
+    /**
+     * Duration of the animation
+     *
+     * @defaultValue 200
+     */
+    transitionDuration?: number;
 
-  /**
-   * Whether to hide the backdrop or not
-   *
-   * @defaultValue false
-   */
-  disableBackdrop?: boolean;
+    /**
+     * Whether to hide the backdrop or not
+     *
+     * @defaultValue false
+     */
+    disableBackdrop?: boolean;
 
-  /**
-   * Disable css transitions
-   *
-   * @defaultValue false
-   */
-  disableTransitions?: boolean;
+    /**
+     * Disable css transitions
+     *
+     * @defaultValue false
+     */
+    disableTransitions?: boolean;
 
-  /**
-   * Whether the focus trap is disabled or not
-   *
-   * @defaultValue false
-   */
-  disableFocusTrap?: boolean;
+    /**
+     * Whether the focus trap is disabled or not
+     *
+     * @defaultValue false
+     */
+    disableFocusTrap?: boolean;
 
-  /**
-   * Focus trap options
-   *
-   * @defaultValue { allowOutsideClick: true }
-   */
-  focusTrapOptions?: unknown;
+    /**
+     * Focus trap options
+     *
+     * @defaultValue { allowOutsideClick: true }
+     */
+    focusTrapOptions?: unknown;
 
-  /**
-   * Whether it is open or not
-   */
-  isOpen: boolean;
+    /**
+     * Whether it is open or not
+     */
+    isOpen: boolean;
 
-  /**
-   * A function that will be called when closed
-   */
-  onClose?: () => void;
+    /**
+     * A function that will be called when closed
+     */
+    onClose?: () => void;
 
-  /**
-   * A function that will be called when closing is finished executing, this
-   * includes waiting for animations/transitions to finish.
-   */
-  didClose?: () => void;
+    /**
+     * A function that will be called when closing is finished executing, this
+     * includes waiting for animations/transitions to finish.
+     */
+    didClose?: () => void;
 
-  /**
-   * A function that will be called when opened
-   */
-  onOpen?: () => void;
+    /**
+     * A function that will be called when opened
+     */
+    onOpen?: () => void;
 
-  /**
-   * Whether to close when the area outside (the backdrop) is clicked
-   *
-   * @defaultValue true
-   */
-  closeOnOutsideClick?: boolean;
+    /**
+     * Whether to close when the area outside (the backdrop) is clicked
+     *
+     * @defaultValue true
+     */
+    closeOnOutsideClick?: boolean;
 
-  /**
-   * Whether to close when the escape key is pressed
-   *
-   * @defaultValue true
-   */
-  closeOnEscapeKey?: boolean;
+    /**
+     * Whether to close when the escape key is pressed
+     *
+     * @defaultValue true
+     */
+    closeOnEscapeKey?: boolean;
 
-  /**
-   * The name of the transition to be used in the backdrop.
-   *
-   * @defaultValue 'overlay-transition--fade'
-   */
-  backdropTransitionName?: string;
+    /**
+     * The name of the transition to be used in the backdrop.
+     *
+     * @defaultValue 'overlay-transition--fade'
+     */
+    backdropTransitionName?: string;
 
-  /**
-   * The name of the transition to be used in the content.
-   *
-   * @defaultValue 'overlay-transition--fade'
-   */
-  contentTransitionName?: string;
+    /**
+     * The name of the transition to be used in the content.
+     *
+     * @defaultValue 'overlay-transition--fade'
+     */
+    contentTransitionName?: string;
+  };
+  Element: HTMLDivElement | null;
 }
 
-export default class Overlay extends Component<OverlayArgs> {
+export default class Overlay extends Component<OverlaySignature> {
   @tracked keepOpen = false;
   contentElement: HTMLElement | undefined;
   mouseDownContentElement: EventTarget | null = null;
