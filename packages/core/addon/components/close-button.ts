991524e9fe148c2790c55ac06dc57e8d0d254d8b
eugenioenko <eyakhnenko@underline.com>
2022-04-06 11:19:16 -0700

    updated close-button signature

--- packages/core/addon/components/close-button.ts
+++ packages/core/addon/components/close-button.ts
@@ -1,21 +1,27 @@
 import Component from '@glimmer/component';
 import { action } from '@ember/object';
 
-export interface CloseButtonArgs {
-  /**
-   * The icon size
-   *
-   * @defaultValue 'lg'
-   */
-  size?: 'xs' | 'sm' | 'md' | 'lg' | 'xl';
+export interface Signature {
+  Args: {
+    /**
+     * The icon size
+     *
+     * @defaultValue 'lg'
+     */
+    size?: 'xs' | 'sm' | 'md' | 'lg' | 'xl';
 
-  /**
-   * The function to call when button is clicked
-   */
-  onClick?: (event: Event) => void;
+    /**
+     * The function to call when button is clicked
+     */
+    onClick?: (event: Event) => void;
+  };
+  Blocks: {
+    default: [string | null];
+  };
+  Element: HTMLButtonElement;
 }
 
-export default class CloseButton extends Component<CloseButtonArgs> {
+export default class CloseButton extends Component<Signature> {
   @action handleClick(event: Event): void {
     if (typeof this.args.onClick === 'function') {
       this.args.onClick(event);
