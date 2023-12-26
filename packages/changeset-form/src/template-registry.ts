39d8ef6ea546a9ee992b7272b8118b33b1fa0011
Josemar Luedke <josemarluedke@gmail.com>
2023-12-26 10:44:54 -0800

    add form and changeset-form components to template-registry

--- packages/changeset-form/src/template-registry.ts
+++ packages/changeset-form/src/template-registry.ts
@@ -1,5 +1,19 @@
-// import type VisuallyHidden from './components/visually-hidden';
+import type ChangesetFormFieldsInput from './components/changeset-form/fields/input';
+import type ChangesetFormFieldsTextarea from './components/changeset-form/fields/textarea';
+import type ChangesetFormFieldsSelect from './components/changeset-form/fields/select';
+import type ChangesetFormFieldsCheckbox from './components/changeset-form/fields/checkbox';
+import type ChangesetFormFieldsCheckboxGroup from './components/changeset-form/fields/checkbox-group';
+import type ChangesetFormFieldsRadio from './components/changeset-form/fields/radio';
+import type ChangesetFormFieldsRadioGroup from './components/changeset-form/fields/radio-group';
+import type ChangesetForm from './components/changeset-form';
 
 export default interface Registry {
-  // VisuallyHidden: typeof VisuallyHidden;
+  ChangesetFormFieldsInput: typeof ChangesetFormFieldsInput;
+  ChangesetFormFieldsTextarea: typeof ChangesetFormFieldsTextarea;
+  ChangesetFormFieldsSelect: typeof ChangesetFormFieldsSelect;
+  ChangesetFormFieldsCheckbox: typeof ChangesetFormFieldsCheckbox;
+  ChangesetFormFieldsCheckboxGroup: typeof ChangesetFormFieldsCheckboxGroup;
+  ChangesetFormFieldsRadio: typeof ChangesetFormFieldsRadio;
+  ChangesetFormFieldsRadioGroup: typeof ChangesetFormFieldsRadioGroup;
+  ChangesetForm: typeof ChangesetForm;
 }
--- packages/forms/src/template-registry.ts
+++ packages/forms/src/template-registry.ts
@@ -1,5 +1,33 @@
-// import type VisuallyHidden from './components/visually-hidden';
+import type FormFieldCheckbox from './components/form-field/checkbox';
+import type FormFieldFeedback from './components/form-field/feedback';
+import type FormFieldHint from './components/form-field/hint';
+import type FormFieldInput from './components/form-field/input';
+import type FormFieldLabel from './components/form-field/label';
+import type FormFieldRadio from './components/form-field/radio';
+import type FormFieldTextarea from './components/form-field/textarea';
+import type FormCheckboxGroup from './components/form-checkbox-group';
+import type FormCheckbox from './components/form-checkbox';
+import type FormField from './components/form-field';
+import type FormInput from './components/form-input';
+import type FormRadioGroup from './components/form-radio-group';
+import type FormRadio from './components/form-radio';
+import type FormSelect from './components/form-select';
+import type FormTextarea from './components/form-textarea';
 
 export default interface Registry {
-  // VisuallyHidden: typeof VisuallyHidden;
+  FormFieldCheckbox: typeof FormFieldCheckbox;
+  FormFieldFeedback: typeof FormFieldFeedback;
+  FormFieldHint: typeof FormFieldHint;
+  FormFieldInput: typeof FormFieldInput;
+  FormFieldLabel: typeof FormFieldLabel;
+  FormFieldRadio: typeof FormFieldRadio;
+  FormFieldTextarea: typeof FormFieldTextarea;
+  FormCheckboxGroup: typeof FormCheckboxGroup;
+  FormCheckbox: typeof FormCheckbox;
+  FormField: typeof FormField;
+  FormInput: typeof FormInput;
+  FormRadioGroup: typeof FormRadioGroup;
+  FormRadio: typeof FormRadio;
+  FormSelect: typeof FormSelect;
+  FormTextarea: typeof FormTextarea;
 }
