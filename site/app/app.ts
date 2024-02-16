ae1d6c2d7e6719edccfd1136a906b33c2adb0c8e
Josemar Luedke <josemarluedke@gmail.com>
2024-02-15 18:55:22 -0800

    chore: add glint to site

--- site/app/app.ts
+++ site/app/app.ts
@@ -2,6 +2,7 @@ import Application from '@ember/application';
 import Resolver from 'ember-resolver';
 import loadInitializers from 'ember-load-initializers';
 import config from 'site/config/environment';
+import '@glint/environment-ember-loose';
 
 export default class App extends Application {
   modulePrefix = config.modulePrefix;
