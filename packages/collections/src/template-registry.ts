0764a40e2ccc2f25e75fd968f48779f5673c74a7
Josemar Luedke <josemarluedke@gmail.com>
2024-03-06 13:40:06 -0800

    chore: use index to import collection components into registry

--- packages/collections/src/template-registry.ts
+++ packages/collections/src/template-registry.ts
@@ -1,5 +1,4 @@
-import type Listbox from './components/listbox';
-import type Dropdown from './components/dropdown';
+import type { Listbox, Dropdown } from './index';
 
 export default interface Registry {
   Listbox: typeof Listbox;
