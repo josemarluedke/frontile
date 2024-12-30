b5736dd6b4b25c031f45180ac736251a6a6113ad
Josemar Luedke <josemarluedke@gmail.com>
2024-12-30 15:28:08 -0800

    chore: update test-app to ember v6

--- test-app/ember-cli-build.js
+++ test-app/ember-cli-build.js
@@ -1,7 +1,6 @@
 'use strict';
 
 const EmberApp = require('ember-cli/lib/broccoli/ember-app');
-const path = require('path');
 
 module.exports = function (defaults) {
   const app = new EmberApp(defaults, {
